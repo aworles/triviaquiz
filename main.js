cab2d1c28ed000e973d38b5218754f361bd52884
aworles <khanhanna.vu2020@gmail.com>
2026-02-03 14:22:44 +0100

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ let quizData = [
     points: 1,
   },
   {
-    question: "What is the *best* Paper 1 unit in the CS syllabus?",
+    question: "What is the best Paper 1 unit in the CS syllabus?",
     options: [
       "Data representation",
       "Boolean Logic",
@@ -61,65 +61,152 @@ let quizData = [
   points: 1,
 },
 
-{
-  question: "What is one of the first homeworks given about Python in MS Teams that not many knew how to complete?",
-  options: ["Bingo Project", "Fibonacci Sequence", "Validation Checks", "1D Arrays"],
-  correct: "Fibonacci Sequence",
-  points: 1,
-},
+  {
+    question: "What is one of the first homeworks given about Python in MS Teams that not many knew how to complete?",
+    options: ["Bingo Project", "Fibonacci Sequence", "Validation Checks", "1D Arrays"],
+    correct: "Fibonacci Sequence",
+    points: 1,
+  },
 
-{
-  question: "What was the first 15 marker (Paper 2) exercise that we looked at about?",
-  options: ["Temperatures", "Mountains", "Matches", "Competitors"],
-  correct: "Temperatures",
-  points: 1,
-},
-
-{
-  question: "What is the exact date of the Y10 End of Year Exam?",
-  options: [
-    "Tuesday 27 May at 13:30",
-    "Wednesday 28 May at 12:45",
-    "Friday 30 May at 13:30",
-    "Monday 26 May at 11:30",
-  ],
-  correct: "Tuesday 27 May at 13:30",
-  points: 1,
-},
-{
-  question: "What is the capital of france?",
-  options: [
-    "Paris",
-    "Marseille",
-    "Lyon",
-    "F",
-  ],
-  correct: "F",
-  points: 1,
-},
-{
-  question: "France's penalty kicks is best described as:",
-  options: [
-    "Elite",
-    "World-class",
-    "Legendary",
-    "Predictable",
-  ],
-  correct: "Predictable",
-  points: 1,
-},
-{
-  question: "French language is described as:",
-  options: [
-    "Romantic",
-    "Elegant",
-    "Beautiful",
-    "English with extra letters",
-  ],
-  correct: "English with extra letters",
-  points: 1,
-},
+  {
+    question: "What was the first 15 marker (Paper 2) exercise that we looked at about?",
+    options: ["Temperatures", "Mountains", "Matches", "Competitors"],
+    correct: "Temperatures",
+    points: 1,
+  },
   
+  {
+    question: "What is the exact date of the Y10 End of Year Exam?",
+    options: [
+      "Tuesday 27 May at 13:30",
+      "Wednesday 28 May at 12:45",
+      "Friday 30 May at 13:30",
+      "Monday 26 May at 11:30",
+    ],
+    correct: "Tuesday 27 May at 13:30",
+    points: 1,
+  },
+  {
+    question: "What is the capital of france?",
+    options: [
+      "Paris",
+      "Marseille",
+      "Lyon",
+      "F",
+    ],
+    correct: "F",
+    points: 1,
+  },
+  {
+    question: "France's penalty kicks is best described as:",
+    options: [
+      "Elite",
+      "World-class",
+      "Legendary",
+      "Predictable",
+    ],
+    correct: "Predictable",
+    points: 1,
+  },
+  {
+    question: "French language is described as:",
+    options: [
+      "Romantic",
+      "Elegant",
+      "Beautiful",
+      "English with extra letters",
+    ],
+    correct: "English with extra letters",
+    points: 1,
+  },
+  {
+    question: "What was the first flowchart exercise about? ",
+    options: [
+      "Input 2 numbers, add them and output the result",
+      "Input 3 nmubers, find the highest and output the result",
+      "Input 2 numbers, multiply them and output the result",
+      "Input 3 numbers, sum them and output the result",
+    ],
+    correct: "Input 2 numbers, multiply them and output the result",
+    points: 1,
+  },
+  {
+    question: "What is the fastest way to make a CS teacher lose hope?",
+    options: [
+      "A student asking what a boolean is ...in Year 11",
+      "A student writing code on a document with no indentation, ..using Comic Sans",
+      "A student using 17 nested IF statements, with no ELIFs",
+      "A student writing a code that asks for 100 inputs without using a loop'"
+    ],
+    correct: "A student asking what a boolean is ...in Year 11",
+    points: 1,
+  },
+  {
+    question: "What is the most common answer for 'state the purpose of the algorithm'?",
+    options: [
+      "Nothing",
+      "Waffling out three sentences",
+      "A detailed explanation of every line of code",
+      "A single sentence describing the purpose using correct keywords"
+    ],
+    correct: "A detailed explanation of every line of code",
+    points: 1,
+  },
+  {
+    question: "Which one is inevitably the best programming language?",
+    options: [
+      "Scratch",
+      "Python",
+      "HTML",
+      "Pseudocode"
+    ],
+    correct: "Python",
+    points: 1,
+  },
+  {
+    question: "54 68 65 20 62 65 73 74 20 67 61 6D 65 3F",
+    options: [
+      "66 6C 69 70 70 79 20 62 69 74 20 61 6E 64 20 74 68 65 20 61 74 74 61 63 6B 20 6F 66 20 74 68 65 20 68 65 78 61 64 65 63 69 6D 61 6C 20 66 72 6F 6D 20 62 61 73 65 20 31 36",
+      "74 68 61 74 20 6F 6E 65 20 6C 69 67 68 74 62 75 6C 62 20 77 65 62 73 69 74 65 20 73 69 72 20 63 72 65 61 74 65 64 20 74 6F 20 74 65 61 63 68 20 62 69 6E 61 72 79",
+      "74 68 69 73 21 20 6F 62 76 69 6F 75 73 6C 79 2E",
+      "77 68 61 74"
+    ],
+    correct: "66 6C 69 70 70 79 20 62 69 74 20 61 6E 64 20 74 68 65 20 61 74 74 61 63 6B 20 6F 66 20 74 68 65 20 68 65 78 61 64 65 63 69 6D 61 6C 20 66 72 6F 6D 20 62 61 73 65 20 31 36",
+    points: 1,
+  },
+  {
+    question: "Why did the chicken cross the road?",
+    options: [
+      "To get to the other side",
+      "To knock on the idiot's door",
+      "To run away from Paper 1",
+      "Because someone's code told it to do so"
+    ],
+    correct: "To get to the other side",
+    points: 1,
+  },
+  {
+    question: "Most efficient way to make a student drop CS",
+    options: [
+      "To get to the other side",
+      "To knock on the idiot's door",
+      "To run away from Paper 1",
+      "Because someone's code told it to do so"
+    ],
+    correct: "To get to the other side",
+    points: 1,
+  },
+  {
+    question: "Approx. how many times did the word 'system' or 'systems' appear in page 243, of the textbook?",
+    options: [
+      "0,
+      "15",
+      "10",
+      "5"
+    ],
+    correct: "15",
+    points: 1,
+  },
   
   
 ];
